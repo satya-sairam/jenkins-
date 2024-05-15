40a14f12ee16600255b59914b5f1d5a1f7102063
satya-sairam <kssairam@redcloudcomputing.com>
2024-05-15 13:41:24 +0530

    added new bash script

--- test.sh
+++ test.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-# Read the new SSH username from Jenkins parameter
+# # Read the new SSH username from Jenkins parameter
+# new_ssh_username=$1
+
+# # Update the orch.yaml file using sed
+# sshUserName=$(grep "sshUserName" orch.yaml | cut -d ':' -f2 | tr -d '[:space:]')
+# replaced_yaml=$(sed "s/sshUserName: .*/sshUserName: $sshUserName/" newoutput.yaml)
+
+# # Create a new file orch-replaced.yaml with the updated content
+# cp orch.yaml orch-replaced.yaml
+
+# # Print out the contents of the replaced file
+# cat orch-replaced.yaml
+
+
+
+
+# Ensure a new SSH username is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <new_ssh_username>"
+    exit 1
+fi
+
+# Read the new SSH username from the command line argument
 new_ssh_username=$1
 
 # Update the orch.yaml file using sed
@@ -11,3 +33,4 @@ cp orch.yaml orch-replaced.yaml
 
 # Print out the contents of the replaced file
 cat orch-replaced.yaml
+
