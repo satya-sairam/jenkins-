249696fa3b92987d3c6432462051fb872ea9cbe7
satya-sairam <kssairam@redcloudcomputing.com>
2024-05-16 19:01:04 +0530

    added new script for testing

---
+++ cluster-automation/parameters.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Function to update the new YAML file
+update_new_yaml() {
+    # Check if correct number of arguments are provided
+    if [ "$#" -ne 5 ]; then
+        echo "Usage: $0 <sshUserName> <sshPassword> <host1> <host2> <host3>"
+        exit 1
+    fi
+
+    # Assigning arguments to variables
+    sshUserName="$1"
+    sshPassword="$2"
+    host1="$3"
+    host2="$4"
+    host3="$5"
+
+    # Convert sshPassword to base64
+    sshPasswordBase64=$(echo -n "$sshPassword" | base64)
+
+    # Perform replacements and save to new file
+    sed -e "s/sshUserName: .*/sshUserName: $sshUserName/" \
+        -e "s/sshPassword: .*/sshPassword: $sshPasswordBase64/" \
+        -e "s/host: \"1.1.1.1\"/host: \"$host1\"/" \
+        -e "s/host: \"2.2.2.2\"/host: \"$host2\"/" \
+        -e "s/host: \"3.3.3.3\"/host: \"$host3\"/" my-default.yaml > new.yaml
+
+    cat my-default.yaml
+
+    echo "-------------------------------"
+
+    cat new.yaml
+}
+
+# Main function
+main() {
+    # Call the function with provided arguments
+    update_new_yaml "$@"
+}
+
+# Call the main function with provided arguments
+main "$@"
+
---
+++ cluster-automation/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Main function
+main() {
+    # Check if folder path is provided
+
+    echo "$1"
+
+}
+
+# Call the main function with the provided folder path argument
+main "$1"
+
